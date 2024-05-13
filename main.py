78592e008d3c15d469c2d76f53de8f7cb9b6a14e
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2024-05-13 15:17:54 -0400

    uma pequena alteração nas mensagens

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ from random import randint
 computer_number = randint(1, 100)
 cont = 0
 
-print("Pensei em um número entre 1 e 100, consegue adivinhar?")
-
 while True:
     print("\n======= JOGO DA ADIVINHAÇÃO =======")
+    print("\nPensei em um número entre 1 e 100, consegue adivinhar?")
     print("\nEm qual número eu pensei?")
     player_guess = int(input("Qual é o seu palpite: "))
     cont +=1
 
     if player_guess > computer_number and player_guess < 101:
-        print(f"O número {player_guess} é maior do que eu escolhi.")
+        print(f"\nO número {player_guess} é maior do que eu escolhi.")
         print("Tente novamente!")
     elif player_guess < computer_number and player_guess > 0:
-        print(f"O número {player_guess} é menor do que eu escolhi.")
+        print(f"\nO número {player_guess} é menor do que eu escolhi.")
         print("Tente novamente!")
     elif player_guess > 100 or player_guess < 1 :
-        print(f"O número {player_guess} é inválido, digite um número entre 1 e 100.")
+        print(f"\nO número {player_guess} é inválido, digite um número entre 1 e 100.")
         print("Tente novamente!")
     else:
-        print(f"\nParabéns!!! Você acertou com {cont} tentativas, eu escolhi o número {computer_number}.")
+        print(f"\nParabéns!!! Você acertou com {cont} tentativas, eu pensei no número {computer_number}.")
         break
